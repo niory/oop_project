de92e39aaa177b1e48b9bd2f58fdef4196fd9c7e
gelyaaa <angelinakoynova@mail.ru>
2023-11-14 15:36:17 +0500

    Изменен класс Main, добавлены компоненты для запуска бота

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,16 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String botUsername = "very_cool_ReminderBot";
+        String botToken = "6799379167:AAFERe3GjtIYBLczcib9FQlHe0fQV-n8tiI";
+
+        ReminderBot bot = new ReminderBot(botUsername, botToken);
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+
+        try {
+            botsApi.registerBot(bot);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 }
