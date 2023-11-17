2eb88d641a59f6bb256730ccb59ed970cdf32ff8
gelyaaa <angelinakoynova@mail.ru>
2023-11-17 14:08:46 +0500

    исправлен класс Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,19 @@
 package org.example;
 
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+
 public class Main {
     public static void main(String[] args) {
         String botUsername = "very_cool_ReminderBot";
         String botToken = "6799379167:AAFERe3GjtIYBLczcib9FQlHe0fQV-n8tiI";
 
-        ReminderBot bot = new ReminderBot(botUsername, botToken);
-        TelegramBotsApi botsApi = new TelegramBotsApi();
-
         try {
+            ReminderBot bot = new ReminderBot(botUsername, botToken);
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+
             botsApi.registerBot(bot);
         } catch (TelegramApiException e) {
             e.printStackTrace();
