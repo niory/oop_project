7d0efda6f1975eb017ce9d60121b4fa5ee27ceda
gelyaaa <angelinakoynova@mail.ru>
2023-12-20 02:40:41 +0500

    добавлен класс HandlerBot для отправки сообщений

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,19 +4,15 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-
 public class Main {
     public static void main(String[] args) {
-        String botUsername = "very_cool_ReminderBot";
-        String botToken = "6799379167:AAFERe3GjtIYBLczcib9FQlHe0fQV-n8tiI";
-
         try {
-            ReminderBot bot = new ReminderBot(botUsername, botToken);
+            ReminderBot bot = new ReminderBot();
             TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-
             botsApi.registerBot(bot);
         } catch (TelegramApiException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
+
         }
     }
 }
